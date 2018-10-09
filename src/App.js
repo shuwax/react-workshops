95d7fbbab6b601f36757939f3212dc7b2be12a23
Marcin Grochulski <marcin@grochulski.net>
2018-10-09 19:37:48 +0200

    Iteration in counter

--- src/App.js
+++ src/App.js
@@ -4,26 +4,40 @@ import Title from './Title';
 import ButtonWithCounter from './ButtonWithCounter'
 
 class App extends Component {
-
     constructor() {
         super();
 
         this.state = {
-            counter: 0
+            counter: 0,
+            iterations: []
         }
     }
 
     handleClick = () => {
-        const {counter} = this.state;
-        this.setState({counter: counter+1});
+        const {counter, iterations} = this.state;
+
+        this.setState({
+            counter: counter + 1,
+            iterations: [...iterations, {
+                id: counter + 1,
+                time: new Date()
+            }]
+        });
     };
 
     render() {
-        const {counter} = this.state;
+        const {counter, iterations} = this.state;
+
+        const list = iterations.map(item => {
+            return <div key={item.id}>{item.id} - {item.time.toString()}</div>;
+        });
+
+
         return (
             <div className="App">
                 <Title type="TEST2" content="Welcome message"/>
                 <ButtonWithCounter onClick={this.handleClick} counter={counter}/>
+                {list}
             </div>
         );
     }
